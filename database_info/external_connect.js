fb437497bad4542a74dfba46316ac4639ba10d9a
OPAabdul <papersopaabdul@gmail.com>
2020-04-05 17:16:26 -0500

    connected to legacy and external db w test routes

---
+++ database_info/external_connect.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'sql9.freesqldatabase.com',
+    database: 'sql9331512',
+    user: 'sql9331512',
+    password: 'ynRxHi4bBG',
+    port: '3306'
+})
+
+connection.connect(function(error) {
+    if (!!error) {
+      console.log('Error');
+    } else {
+      console.log('Connected');
+    }
+  });
+
+module.exports = connection;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+let legacy_connect = require('../database_info/legacy_connect');
+let external_connect = require('../database_info/external_connect');
+
+router.get('/getAllCustomers', function(req, res) {
+  var query = "SELECT * FROM customers";
+  legacy_connect.query(query, (err, results, fields) => {
+    if(err)
+      return console.error(err.message);
+
+    res.send(results);
+  });
+});
+
+router.get('/test', function(req, res) {
+  var query = "SELECT * FROM test";
+  external_connect.query(query, (err, results, fields) => {
+    if(err)
+      return console.error(err.message);
+
+    res.send(results);
+  });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
