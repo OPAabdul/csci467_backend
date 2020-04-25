97fb5165fafda132d4232e7b9f523c640ce28728
OPAabdul <papersopaabdul@gmail.com>
2020-04-25 16:55:54 -0500

    added quotes routes

--- routes/quotes_lines.js
+++ routes/quotes_lines.js
@@ -4,21 +4,36 @@ var bodyParser = require('body-parser');
 
 let external_connect = require('../database_info/external_connect');
 
+router.use(bodyParser.urlencoded({extended: false}));
+
 router.post('/createQuote', function (req,res) {
     var query = "INSERT INTO quotes(cid, aid, status, email) ";
     query += "values(?,?,0,?)";
-    // external_connect.query(query, [req.params.cid,req.params.aid,req.params.email], (err, results, fields) => {
-    //     if (err)
-    //         console.error("failed to insert quote: " + err.message);
+    external_connect.query(query, [req.body.cid,req.body.aid,req.body.email], (err, results, fields) => {
+        if (err)
+            console.error("failed to insert quote: " + err.message);
 
-    //     res.end();
-    // }); 
-    console.log(req.params);
+        res.end();
+    }); 
+    console.log(req.body);
 });
 
 router.get('/quotetest', function (req,res) {
     console.log('quote test');
+    res.end();
 })
+
+router.get('/quotes_info', function (req, res) {
+    var query = "SELECT quotes.qid, customers.name AS cname, associates.name AS aname, ";
+    query += "quotes.total_price, quotes.email, quotes.date_ordered FROM quotes ";
+    query += "INNER JOIN customers ON quotes.cid = customers.id INNER JOIN associates ON quotes.aid = associates.aid;";
+    external_connect.query(query, (err, results, fields) => {
+        if (err)
+            return console.error(err.message)
+
+        res.send(results);
+    });
+});
 
 router.get('/quotes', function (req, res) {
     var query = "SELECT * FROM quotes";
