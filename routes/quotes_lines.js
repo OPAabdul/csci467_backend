ffb9d29c030000fe2d2a3cbf5bf208d43ad346a7
OPAabdul <papersopaabdul@gmail.com>
2020-04-25 19:24:29 -0500

    fixed queries

--- routes/quotes_lines.js
+++ routes/quotes_lines.js
@@ -45,8 +45,51 @@ router.get('/quotes', function (req, res) {
     });
 });
 
+router.get('/quote/:email/:cid/:aid', function (req, res) {
+    var query = "SELECT * FROM quotes WHERE email=? AND cid=? AND aid=? ORDER BY date_ordered DESC";
+    external_connect.query(query, [req.params.email,req.params.cid,req.params.aid], (err, results, fields) => {
+        if (err)
+            return console.error(err.message)
+        console.log(results);
+        res.send(results);
+    });
+});
+
+router.get('/quote/:id', function (req, res) {
+    var query = "SELECT * FROM quotes WHERE qid=?";
+    external_connect.query(query, [req.params.id], (err, results, fields) => {
+        if (err)
+            return console.error(err.message)
+
+        res.send(results);
+    });
+});
+
+router.post('/createLine', function (req,res) {
+    var query = "INSERT INTO listi(qid, descr, price, secret_notes) ";
+    query += "values(?,?,?,?)";
+    var doublePrice = parseFloat(req.body.price);
+    external_connect.query(query, [req.body.qid,req.body.descr,doublePrice,req.body.notes], (err, results, fields) => {
+        if (err)
+            console.error("failed to insert line: " + err.message);
+
+        res.end();
+    }); 
+    // console.log(req.body);
+});
+
+router.get('/lines', function (req, res) {
+    var query = "SELECT * FROM listi";
+    external_connect.query(query, (err, results, fields) => {
+        if (err)
+            return console.error(err.message)
+
+        res.send(results);
+    });
+});
+
 router.get('/lineItems/:qid', function (req, res) {
-    var query = "SELECT lid, descr, price, secret_notes FROM listi INNER JOIN quotes ON listi.qid = quotes.qid WHERE qid = ?";
+    var query = "SELECT lid, descr, price, secret_notes FROM listi INNER JOIN quotes ON listi.qid = quotes.qid WHERE listi.qid = ?";
     external_connect.query(query, [req.params.qid], (err, results, fields) => {
         if (err)
             return console.error(err.message);
