5e3edfdfb917993a611edbae894bb65ed6e2ec2e
OPAabdul <papersopaabdul@gmail.com>
2020-04-26 18:39:44 -0500

    delete associate route

--- routes/associates.js
+++ routes/associates.js
@@ -26,6 +26,16 @@ router.get('/associate/:id', function (req, res) {
     });
 });
 
+router.delete('/associate/:id', function (req, res) {
+    var query = "DELETE FROM associates WHERE aid=?";
+    external_connect.query(query, [req.params.id], (err, results, fields) => {
+        if (err)
+            return console.error(err.message)
+
+        res.end();
+    });
+});
+
 router.put('/updateAssociate/:id/:user/:pass/:addr', function (req, res) {
     var query = "UPDATE associates SET username=?, password=?, address=? WHERE aid=?";
     external_connect.query(query, [req.params.user,req.params.pass,req.params.addr,req.params.id], (err, results, fields) => {
