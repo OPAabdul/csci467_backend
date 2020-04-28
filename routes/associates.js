1dd5619250fb49ae234c65b7aec5e92acfe97c88
Marjo <marjofeliciano@gmail.com>
2020-04-28 08:45:45 -0500

    Added login page backend route

--- routes/associates.js
+++ routes/associates.js
@@ -6,6 +6,16 @@ let external_connect = require('../database_info/external_connect');
 
 router.use(bodyParser.urlencoded({extended: false}));
 
+router.get('/auth', function (req, res) {
+    var query = "SELECT * FROM associates WHERE username=?";
+    external_connect.query(query, [req.query.username], (err, results, fields) => {
+        if (err)
+            return console.error(err.message)
+
+        res.send(results);
+    });
+});
+
 router.get('/associates', function (req, res) {
     var query = "SELECT * FROM associates";
     external_connect.query(query, (err, results, fields) => {
